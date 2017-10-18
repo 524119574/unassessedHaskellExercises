cc74b7f633bcd7aa237f2b149a3f941b4abe5106
Wang Ge <wg116@voxel06.doc.ic.ac.uk>
2017-10-18 18:03:19 +0100

    'very productive' finish one function!

--- Set3.hs
+++ Set3.hs
@@ -2,12 +2,12 @@ import Data.List
 -- question 9
 -- strange, not the result that I expected
 perms :: Eq a => [a] -> [[a]]
-perms [] = []
-perms ls = concat [addHead x (perms (ls\\[x])) | x <- ls]
-
-addHead :: a -> [[a]] -> [[a]]
-addHead x [] = [[x]]
-addHead x (y:ys) =  (x:y):addHead x ys
+perms [a] = [[a]]
+perms ls = concat [addHead x (perms (ls \\ [x])) | x <- ls]
+  where
+    addHead :: a -> [[a]] -> [[a]]
+    addHead _ [] = []
+    addHead x (y:ys) =  (x:y):addHead x ys
 
 
 -- question 10
