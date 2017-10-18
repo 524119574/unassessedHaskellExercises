8138056bed513eef9620da27488a3ff43f27e3d6
Wang Ge <wg116@voxel06.doc.ic.ac.uk>
2017-10-18 18:41:23 +0100

    add prefixes

--- Set3.hs
+++ Set3.hs
@@ -1,13 +1,30 @@
 import Data.List
+-- question 6
+prefixes :: [t] -> [[t]]
+prefixes [] = []
+prefixes (x:xs) = addElem x (prefixes xs)
+  where
+    addElem :: a ->[[a]]->[[a]]
+    addElem x [] = [[x]]
+    addElem x (y:ys) = (x:y):addElem x ys
+
+-- question 8
+subString :: String -> [String]
+subString = error "subString is still undefined yet"
+
 -- question 9
 -- strange, not the result that I expected
 perms :: Eq a => [a] -> [[a]]
 perms [a] = [[a]]
 perms ls = concat [addHead x (perms (ls \\ [x])) | x <- ls]
-  where
-    addHead :: a -> [[a]] -> [[a]]
-    addHead _ [] = []
-    addHead x (y:ys) =  (x:y):addHead x ys
 
 
 -- question 10
+
+
+addHead :: a -> [[a]] -> [[a]]
+addHead _ [] = []
+-- uncomment below line if you want to get all permutation from length 1 to
+-- n
+-- addHead x [] = [[x]]
+addHead x (y:ys) =  (x:y):addHead x ys
